c118a4a2a510c8014c19bd36d1b736289693508a
MinerNik <sna9567@gmail.com>
2022-11-24 21:41:42 +0300

    topJava27_7_16_soapui_utf8_converter.patch

--- src/main/java/ru/javawebinar/topjava/web/json/JacksonObjectMapper.java
+++ src/main/java/ru/javawebinar/topjava/web/json/JacksonObjectMapper.java
@@ -19,7 +19,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 public class JacksonObjectMapper extends ObjectMapper {
 
     private static final ObjectMapper MAPPER = new JacksonObjectMapper();
-    private static final ObjectMapper MAPPER = new JacksonObjectMapper();
 
     private JacksonObjectMapper() {
         registerModule(new Hibernate5Module());
